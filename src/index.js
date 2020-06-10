907c2359bc3cace4b6c4540cc9d172debcca28b9
caiotriana <caio_triana@hotmail.com>
2020-06-10 19:58:53 -0300

    reestructure foldes and name files on project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Lettering from './App';
+import Lettering from './pages/Lettering';
 import GlobalStyle from './style'
 
 ReactDOM.render(
---
+++ src/pages/style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+const ContentTitleName = styled.div`
+    height : 100vh;
+    width : 100vw;
+    display : flex;
+    justify-content : space-between;
+    padding : 24px;
+    position : fixed;
+    align-items : center;
+    left :  -${props => props.showLastName}px;
+`
+
+const TitleName = styled.h1`
+    font-family: Archivo;
+    font-size : ${props => props.passFontSize}px;
+    color : #fff;
+    white-space: pre;
+    position: relative;
+
+     &:after {
+        content: "";
+        position: absolute;
+        left: 0;
+        z-index: -1;
+        height : ${props => props.passFontSize*6}px;
+        width  : ${props => props.passFontSize*6}px;
+        background : #000;
+        top : 50%;
+        border-radius : 5000px;
+        transform : translateY(-50%);
+  }
+`
+
+export {ContentTitleName, TitleName }
